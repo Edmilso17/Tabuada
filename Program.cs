612c92c01ae613437a9e3821b1907ae65cabfd85
Edmilson <edmilsonribeiro.17072005@gmail.com>
2022-05-20 21:48:48 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿double contador, numero, produto;
+int multiplicaçao;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Digite a tabuada desejada: ");
+numero = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("Digite a multiplicação da tabuada desejada: ");
+multiplicaçao = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+if(numero <0 || numero >10)
+{
+    Console.WriteLine("Digite um numero de 0 a 10");
+}
+
+else
+{
+    contador=0;
+    while(contador <= multiplicaçao)
+    {
+        produto = contador * numero;
+        Console.WriteLine($"{numero} x {contador} = {produto}");
+        contador+=1;
+    }
+}
